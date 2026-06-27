68a7ca3e4e708de5a0423ad1c9499d84787ca671
pedrocassol <cassolpedro61@gmail.com>
2026-06-27 13:54:45 -0300

    projeto inicial gerado pelo spring initializr

---
+++ src/main/java/br/com/petcare/PetcareSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcareSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcareSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petcare/PetcareSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
